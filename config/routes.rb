e0a800d253f04637e215d9440edfb80b67596e79
guiatsu <yuricrystian@hotmail.com>
2018-11-23 12:31:28 -0200

    arrumado o bug do botão não funcionar no firefox

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'order_meals/index'
-  get 'order_meals/show'
-  get 'order_meals/edit'
-  get 'order_meals/new'
+  get 'situations/index'
+  get 'situations/show'
+  get 'situations/edit'
+  get 'situations/new'
   resources :meals
   resources :categories
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
