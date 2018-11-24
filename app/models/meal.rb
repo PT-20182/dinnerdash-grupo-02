efad827429b7ddbda016ea74983c8b90d7574bc0
Bruno Sanguinetti <brunoebarros@gmail.com>
2018-11-24 18:04:51 -0200

    finalizando model, meals

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,4 +1,7 @@
 class Meal < ApplicationRecord
+    validates :name, presence: true, length: { minimum: 2}
+    validates :description, presence: true, length: { minimum: 2}
+    validates :price, presence: true, numericality: { greater_than: 0 }
     belongs_to :category , required: false
     has_one_attached :image
 end
