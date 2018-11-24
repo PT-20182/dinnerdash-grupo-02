3cd4bb5cc0aeff0aab1195460ea2117a4cfc8787
guiatsu <yuricrystian@hotmail.com>
2018-11-24 19:31:02 -0200

    alterei as rotas

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'situations/index'
-  get 'situations/show'
-  get 'situations/edit'
-  get 'situations/new'
   resources :meals
   resources :categories
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
