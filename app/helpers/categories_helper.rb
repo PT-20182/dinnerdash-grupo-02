c7b5b6fbe3b10b471e5c081acda3d356792cff1e
guiatsu <yuricrystian@hotmail.com>
2018-11-25 18:07:21 -0200

    estilização da tela de login

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,11 @@
 module CategoriesHelper
+	def quantidade(category)
+		i = 0 
+		@meals.each do | meal |
+			if(meal.category_id == category.id)
+				i += 1 
+			end
+		end
+		return i
+	end
 end
--- app/models/meal.rb
+++ app/models/meal.rb
@@ -2,6 +2,7 @@ class Meal < ApplicationRecord
     validates :name, presence: true, length: { minimum: 2}
     validates :description, presence: true, length: { minimum: 2}
     validates :price, presence: true, numericality: { greater_than: 0 }
+    validates :available, presence: false
     belongs_to :category , required: false
     has_many :order_meals
     has_one_attached :image
