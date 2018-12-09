9511ae44c62b585eb0d383ca8e2669f0b656cadf
Bia Sanguinetti <Bia@MacBook-Air-de-Bia.local>
2018-12-09 19:50:52 -0200

    trabalhando cards

--- app/models/order_meal.rb
+++ app/models/order_meal.rb
@@ -1,11 +1,5 @@
 class OrderMeal < ApplicationRecord
-	validades: :quantity precense: true, numericality: { only_integer:true, greater_than: 0 }
-
+    validates :quantity, presence: true, numericality: { only_integer:true, greater_than: 0 }
 	belongs_to :meal
 	belongs_to :order
-
-
-	def unit_price
-		if persisted?
-			self[]
 end
