5e6664b0bd22445b15e6e7623dc3c4c04178e3e2
guiatsu <yuricrystian@hotmail.com>
2018-12-10 20:18:25 -0200

    consertado a seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-Situation.create! id: 1, status: "Em andamento"
-Situation.create! id: 2, status: "Pago"
-Situation.create! id: 3, status: "Cancelado"
-Situation.create! id: 4, status: "Confirmado"
+Situation.create(status: "Em andamento")
+Situation.create(status: "Pago")
+Situation.create(status: "Cancelado")
+Situation.create(status: "Confirmado")
