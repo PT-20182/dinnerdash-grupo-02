6e88d7b8277efa07bc31e4c75a53a95483c467b0
guiatsu <yuricrystian@hotmail.com>
2018-12-11 08:12:46 -0200

    adcionado admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,9 @@ Situation.create(status: "Pago")
 Situation.create(status: "Cancelado")
 Situation.create(status: "Confirmado")
 User.delete_all
+User.create! do |admin|
+	admin.email = 'admin@gmail.com'
+	admin.password = '123456'
+	admin.password_confirmation = '123456'
+	admin.admin = true
+end
