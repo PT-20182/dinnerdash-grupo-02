6467b5fb90b0746293f5d3f649f2210bc6a110be
Bia Sanguinetti <Bia@MacBook-Air-de-Bia.local>
2018-12-15 17:08:00 -0200

    editar usuario

--- app/controllers/backoffice_users_controller.rb
+++ app/controllers/backoffice_users_controller.rb
@@ -1,4 +1,5 @@
 class BackofficeUsersController < ApplicationController
+  before_action :current_user, only:[:show, :edit, :update, :destroy]
   def index
     @users = User.all
   end
@@ -7,8 +8,18 @@ class BackofficeUsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def edit
+  end
+
+  def destroy 
+   @user.destroy
+
+   redirect_to backoffice_users_path, notice: "Deletado com sucesso!"
+  end
+  def current_user
+    @user = User.find(params[:id])
   end
 end
